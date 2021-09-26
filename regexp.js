381039ea8676fe768bffb44372eff4ae2c276d6f
marielag04 <89762912+marielag04@users.noreply.github.com>
2021-09-25 21:50:21 -0500

    Add files via upload

---
+++ regexp.js
@@ -0,0 +1,10 @@
+function testRegExp(){
+    const regExp = document.getElementById();
+    const globalRegExp = document.getElementById();
+    const test = document.getElementById();
+    if (input1=input2){
+        true;
+    } else {
+        false;
+    }
+}
